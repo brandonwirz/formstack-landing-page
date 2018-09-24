a4e1700300d217437522e5450bbddfe357428b6e
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-09-24 02:30:37 -0600

    updating css

--- js/app.js
+++ js/app.js
@@ -22,11 +22,7 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('#photo-gallery').offset().top
       }, 'slow', 'swing');
     });
-    // scroll to the top
-    $('#top').click(function(){
-        $('html, body').animate({scrollTop: $('grid-container').offset().top
-      }, 'fast', 'swing');
-    });
+
     // scroll to the intro
     $('#intro-bottom').click(function(){
         $('html, body').animate({scrollTop: $('#co-skiing').offset().top
@@ -42,16 +38,21 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('#contact-form').offset().top
       }, 'fast', 'swing');
     });
+    // scroll to the top
+    // $('#top').click(function(){
+    //     $('html, body').animate({scrollTop: $('grid-container').offset().top
+    //   }, 'fast', 'swing');
+    // });
 });
 
 // JS Modal images -------------------------------------------------------------
 var modal = document.getElementById('modal-image');
 var img = document.getElementById('thumb1');
-var modalImg = document.getElementById("img01");
+// var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    // modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
 
@@ -62,11 +63,11 @@ span.onclick = function() {
 // modal 2
 var modal2 = document.getElementById('modal-image2');
 var img2 = document.getElementById('thumb2');
-var modalImg2 = document.getElementById("img02");
+// var modalImg2 = document.getElementById("img02");
 var captionText2 = document.getElementById("caption2");
 img2.onclick = function(){
     modal2.style.display = "block";
-    modalImg2.src = this.src;
+    // modalImg2.src = this.src;
     captionText2.innerHTML = this.alt;
 }
 
@@ -77,11 +78,11 @@ span2.onclick = function() {
 // modal 3
 var modal3 = document.getElementById('modal-image3');
 var img3 = document.getElementById('thumb3');
-var modalImg3 = document.getElementById("img03");
+// var modalImg3 = document.getElementById("img03");
 var captionText3 = document.getElementById("caption3");
 img3.onclick = function(){
     modal3.style.display = "block";
-    modalImg3.src = this.src;
+    // modalImg3.src = this.src;
     captionText3.innerHTML = this.alt;
 }
 
@@ -92,11 +93,11 @@ span3.onclick = function() {
 // modal 4
 var modal4 = document.getElementById('modal-image4');
 var img4 = document.getElementById('thumb4');
-var modalImg4 = document.getElementById("img04");
+// var modalImg4 = document.getElementById("img04");
 var captionText4 = document.getElementById("caption4");
 img4.onclick = function(){
     modal4.style.display = "block";
-    modalImg4.src = this.src;
+    // modalImg4.src = this.src;
     captionText4.innerHTML = this.alt;
 }
 
@@ -107,11 +108,11 @@ span4.onclick = function() {
 // modal 65
 var modal5 = document.getElementById('modal-image5');
 var img5 = document.getElementById('thumb5');
-var modalImg5 = document.getElementById("img05");
+// var modalImg5 = document.getElementById("img05");
 var captionText5 = document.getElementById("caption5");
 img5.onclick = function(){
     modal5.style.display = "block";
-    modalImg5.src = this.src;
+    // modalImg5.src = this.src;
     captionText5.innerHTML = this.alt;
 }
 
@@ -122,11 +123,11 @@ span5.onclick = function() {
 // modal 6
 var modal6 = document.getElementById('modal-image6');
 var img6 = document.getElementById('thumb6');
-var modalImg6 = document.getElementById("img06");
+// var modalImg6 = document.getElementById("img06");
 var captionText6 = document.getElementById("caption6");
 img6.onclick = function(){
     modal6.style.display = "block";
-    modalImg6.src = this.src;
+    // modalImg6.src = this.src;
     captionText6.innerHTML = this.alt;
 }
 
